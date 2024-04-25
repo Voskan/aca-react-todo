0e29865f0708d7c9097a6b2cd102b9c3c9a53179
Voskan Voskanyan <voskanvoskanyan@yandex.com>
2024-04-25 22:51:03 +0400

    initial commit

---
+++ src/TodoList/TodoList.jsx
@@ -0,0 +1,17 @@
+import TodoListItem from "./TodoListItem";
+
+const TodoList = () => {
+  return (
+    <ul>
+      <TodoListItem />
+      <TodoListItem />
+      <TodoListItem />
+      <TodoListItem />
+      <TodoListItem />
+      <TodoListItem />
+      <TodoListItem />
+    </ul>
+  );
+};
+
+export default TodoList;
